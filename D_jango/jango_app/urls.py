b82d97770e673f3168cfe49d7405823ed29e2c5f
Aritra <aritrarakshit2003@gmail.com>
2024-06-18 10:43:50 +0530

    Add Form nad changed urls

--- D_jango/jango_app/urls.py
+++ D_jango/jango_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_jango, name="all_jango"),
     path('<int:chai_id>/', views.chai_description, name="chai_description"),
+    path('stores/', views.chai_store, name="chai_store"),
 ]
